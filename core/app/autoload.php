94338f8e26dab41da91d0024041503709f9d2b14
Carlos230600 <60777696+Carlos230600@users.noreply.github.com>
2022-01-20 09:11:37 +0100

    Update autoload.php

--- core/app/autoload.php
+++ core/app/autoload.php
@@ -4,12 +4,12 @@
 // esta funcion elimina el hecho de estar agregando los modelos manualmente
 
 
-function spl_autoload_register($modelname){
+function my_autoload($modelname){
 	if(Model::exists($modelname)){
 		include Model::getFullPath($modelname);
 	} 
 }
 
-
+myAutoload('my_autoload');
 
 ?>
